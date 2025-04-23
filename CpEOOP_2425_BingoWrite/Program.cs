2370a5157a4ba38a7ea7d23d054d502717509473
24-0315c <24-0315c@CCL3-09>
2025-04-23 13:47:14 +0800

    yes

--- CpEOOP_2425_BingoWrite/Program.cs
+++ CpEOOP_2425_BingoWrite/Program.cs
@@ -8,16 +8,43 @@ namespace CpEOOP_2425_BingoWrite
 {
     internal class Program
     {
+
+        //NumAsker
+        static void NumAsker(out int num)
+        {
+
+            while (true) // ask until valid
+            {
+                Console.Write("Enter number: ");
+                int.TryParse(Console.ReadLine(), out num);
+                if (num > 0)
+                {
+                    break;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            BINGOCard bc = new BINGOCard();
+            Console.WriteLine("Please enter number of cards:");
+            NumAsker(out int num);
+            BINGOCard[] cards = new BINGOCard[num];
+
             FileManager fm = new FileManager();
-            bc.Initialize();
-            bc.GenerateCard();
-            bc.Display();
 
-            fm.FileWrite("Bingo.txt",bc.Stringify());
+            for (int x = 0; x < cards.Length; x++)
+            {
+                cards[x] = new BINGOCard();
+                cards[x].Initialize();
+                cards[x].GenerateCard();
 
+                //FileWrite - file write the bingo cards
+                fm.FileWrite($"C:\\Users\\24-0315c\\Downloads\\BingoCard{x + 1}.csv", cards[x].Stringify(), false);
+
+                Console.WriteLine($"Generated Bingo Card #{x + 1}");
+                cards[x].Display();
+
+            }
         }
     }
 }
+
